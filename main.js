1811d1715fad710432cd5e780692ebdfa5441bb9
BaransCoding <79487593+BaransCoding@users.noreply.github.com>
2021-06-29 18:04:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    postNet = ml5.postNet(video, modelLoaded);
+    postNet.on('pose', gotPoses);
+}
+
+function gotPoses(result){
+if(results.length > 0)
+{
+    console.log(results);
+    console.log("nose x =" + results[0].pose.nose.x);
+    console.log("nose y =" + results[0].pose.nose.y);
+}
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+ }
+
+function take_Snapshot(){
+    save('myFilterImage.png');
+}
+
+function preload(){
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
